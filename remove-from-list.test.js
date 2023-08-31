d9b96d2cb63e200fe03d41a2b5092442f126e5cd
Mila <141172279+Milana56@users.noreply.github.com>
2023-08-31 17:51:29 +0600

    Update remove-from-list.test.js

--- remove-from-list.test.js
+++ remove-from-list.test.js
@@ -1,4 +1,4 @@
-asdfasdfasdvzxcvzxcvasdfasdfaszxcvzxcvvzxcvsadfasdfasadfasdfadfadsfasd////const { assert } = require('chai');
+asdfasdfasdasdfasdfasdvzxcvzxcvasdfasdfaszxcvzxcvvzxcvsadfasdfasadfasdfadfadsfasd////const { assert } = require('chai');
 const { testOptional, ListNode } = require('../extensions/index.js');
 const { removeKFromList } = require('../src/remove-from-list.js');
 
